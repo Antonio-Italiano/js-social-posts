b9f09afc155bac7d8e12625950fddf852895a36c
Antonio-Italiano <antoitalia221@gmail.com>
2022-12-23 15:44:50 +0100

    Milestone 2 Create Post Loop

--- js/script.js
+++ js/script.js
@@ -94,3 +94,45 @@ const postObjects =[
 ]
 
 
+// CREO UN CICLO PER STAMPARE I POST CON LE INFORMAZIONI DELL'ARRAY 
+let contentCard = '';
+postObjects.forEach((element, i) => {
+
+    contentCard += `
+         
+    <div class="post">
+     <div class="post__header">
+      <div class="post-meta">
+        <div class="post-meta__icon">
+          <img class="profile-pic" src="img/${element.profilePicture}" alt="${element.name}" />
+        </div>
+        <div class="post-meta__data">
+          <div class="post-meta__author">${element.name}</div>
+          <div class="post-meta__time">${element.date}</div>
+        </div>
+      </div>
+    </div>
+    <div class="post__text">
+    ${element.text}
+    </div>
+    <div class="post__image">
+      <img src="img/${element.postImg}" alt="" />
+    </div>
+    <div class="post__footer">
+      <div class="likes js-likes">
+        <div class="likes__cta">
+          <button class="like-button js-like-button" href="#" data-postid="1">
+            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+            <span class="like-button__label">Mi Piace</span>
+          </button>
+        </div>
+        <div class="likes__counter">Piace a <b id="like-counter-1" class="js-likes-counter">${element.like}</b> persone</div>
+      </div>
+    </div>
+  </div>
+          
+    `;
+});
+
+// STAMPO NEL DOM 
+cardElement.innerHTML = contentCard;
